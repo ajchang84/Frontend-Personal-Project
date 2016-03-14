97fbb7ac4d99117a6e876e28483e9e7804b2dcf9
Alexander Chang <gxchang@gmail.com>
2016-03-14 16:26:19 -0700

    draft layout and AJAX calls completed

--- script.js
+++ script.js
@@ -1,40 +1,46 @@
 $(document).ready(function(){
 
-  $('form').on('submit', function(e){
-
+  $('#pokemonA').on('submit', function(e){
     e.preventDefault();
-    $.ajax({
-      method: "GET",
-      url: "http://pokeapi.co/api/v1/pokemon/" + $('input').val(),
-      dataType: "json",
-      success: function(data){
-        $('.data').empty();
-        $('.data').append('<p> name: ' + data.name + '</p>');
-        $('.data').append('<p> special attack: ' + data.sp_atk + '</p>');
-        $('.data').append('<p> special defense: ' + data.sp_def + '</p>');
-        $('.data').append('<p> hp: ' + data.hp + '</p>');
-        $('.data').append('<p> speed: ' + data.speed + '</p>');
-        $('.data').append('<p> attack: ' + data.attack + '</p>');
-        $('.data').append('<p> defense: ' + data.defense + '</p>');
-        $sprite = data.sprites[0].resource_uri;
-        $('#pokemonA img').attr("src", data.sprites[0].resource_uri.image);
-        for (var i = 0; i < data.types.length; i++) {
-        $('.data').append('<p> types: ' + data.types[i].name + '</p>');
-        }
-      },
-      // error: function ajaxFailure(data){
-      //   $('ul').append('<li>' + console.log(data.Error) + '</li>');
-      // // },
-      // data: {
-      //   s: $('#movie').val()
-      // }
-    });
+    queryPAI('A', $('#pokemonA input').val());
   });
 
-  // // $('ul').on('click', 'a', function(e){
-  // //   e.preventDefault();
-  // //   $('img').attr('src', $(this).attr('href'));
-  // });
+  $('#pokemonB').on('submit', function(e){
+    e.preventDefault();
+    queryPAI('B', $('#pokemonB input').val());
+  });
+
+  function queryPAI(pokemon, val) {
+    var stats = `.stats${pokemon}`;
+    var sprite = `#sprite${pokemon}`;
+    $.ajax({
+      method: "GET",
+      url: "http://pokeapi.co/api/v1/pokemon/" + val,
+      dataType: "json",
+      success: function(data){
+        $(stats).empty();
+        $(stats).append('<p> name: ' + data.name + '</p>');
+        $(stats).append('<p> hp: ' + data.hp + '</p>');
+        $(stats).append('<p> attack: ' + data.attack + '</p>');
+        $(stats).append('<p> defense: ' + data.defense + '</p>');
+        $(stats).append('<p> special attack: ' + data.sp_atk + '</p>');
+        $(stats).append('<p> special defense: ' + data.sp_def + '</p>');
+        $(stats).append('<p> speed: ' + data.speed + '</p>');
+        $sprite = data.sprites[0].resource_uri;
+        for (var i = 0; i < data.types.length; i++) {
+          $(stats).append('<p> types: ' + data.types[i].name + '</p>');
+        }
+        $.ajax({
+          method: "GET",
+          url: "http://pokeapi.co" + $sprite,
+          dataType: "json",
+          success: function(data){
+            $(sprite).css("background", "url(http://pokeapi.co" + data.image +") 50% 50% no-repeat");
+          }
+        });
+      }
+    });
+  }
 
 });
 
